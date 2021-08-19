8fabf5ab75ec5c926df5d10557c1f71f9898e3ed
luizsilva <luizfsilva.091595@gmail.com>
2021-08-19 11:27:09 -0400

    cleaned up workoutList component, added enviroment folders

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Getting Started with Create React App
+# Final Project: still in development....
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
--- src/components/WorkoutList.js
+++ src/components/WorkoutList.js
@@ -5,11 +5,10 @@ import { useParams } from 'react-router-dom'
 export default function WorkoutList() {
   const { level } = useParams()     // level is being deconstructed to be dynamic to whichever Link is being clicked (App.js line:13 is parameter ":level") and it is Linked in Home.js on the Link that shows "/workout/beginner" for example"
   const [ workouts, setWorkouts ] = useState([])
-  // const [ finish, setFinish ] = useState(false)
 
   // DO A USE EFFECT TO SEND THE LEVEL TO THE BACKEND API
   useEffect(() => {
-    fetch('http://localhost:5000/workouts', {
+    fetch(`${process.env.REACT_APP_API_ENDPOINT}/workouts`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -23,23 +22,10 @@ export default function WorkoutList() {
 
   console.log('this is the full list of workouts from state-->', workouts)   // "workouts" is now the full list of workouts we get based off the level we clicked
 
-  useEffect(() => {
-    fetch('http://localhost:5000/workouts', {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(),
-    })
-    .then(res => res.json())
-    .then(data => console.log('this is my data--->', data))
-    .catch(err => console.log(err))
-  }, [])
-
   return (
     <ul>
-    {workouts.map((workout, i) => {     // mapping through the list of workouts, setting the data into single variable "workout"
-      return <li key={i}><Button variant="dark">done</Button> &nbsp; {`name: ${workout.name}/ sets: ${workout.sets}/ reps: ${workout.reps}/`}</li>   // then returning it as a list dynamically by category: DISPLAYING IT TO SITE
+    {workouts.map((exercise, i) => {     // mapping through the list of exercises, setting the data into single variable "exercise"
+      return <li key={i}><Button variant="dark">done</Button> &nbsp; {`name: ${exercise.name}/ sets: ${exercise.sets}/ reps: ${exercise.reps}/`}</li>   // then returning it as a list dynamically by category: DISPLAYING IT TO SITE
     })}
   </ul>
   )
