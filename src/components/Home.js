a615e2dcfc438f4cf52703f67cbbe4994ba4ea1a
luizsilva <luizfsilva.091595@gmail.com>
2021-08-20 09:12:04 -0400

    added handleClick function so that we can complete exercise from false to true

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,7 +15,7 @@ function Home() {
               bulk of the card's content.
             </Card.Text>
             <Link to='/workout/beginner'>
-              <Button variant='primary'>TRY</Button>
+              <Button variant='primary'>START</Button>
             </Link>
           </Card.Body>
         </Card>
@@ -28,7 +28,7 @@ function Home() {
               bulk of the card's content.
             </Card.Text>
             <Link to='/workout/intermediate'>
-              <Button variant='primary'>TRY</Button>
+              <Button variant='primary'>START</Button>
             </Link>
           </Card.Body>
         </Card>
@@ -41,7 +41,7 @@ function Home() {
               bulk of the card's content.
             </Card.Text>
             <Link to='/workout/expert'>
-              <Button variant='primary'>TRY</Button>
+              <Button variant='primary'>START</Button>
             </Link>
           </Card.Body>
         </Card>
--- src/components/WorkoutList.js
+++ src/components/WorkoutList.js
@@ -5,6 +5,7 @@ import { useParams } from 'react-router-dom'
 export default function WorkoutList() {
   const { level } = useParams()     // level is being deconstructed to be dynamic to whichever Link is being clicked (App.js line:13 is parameter ":level") and it is Linked in Home.js on the Link that shows "/workout/beginner" for example"
   const [ workouts, setWorkouts ] = useState([])
+  const  [exerciseName, setExerciseName] = useState("")
 
   // DO A USE EFFECT TO SEND THE LEVEL TO THE BACKEND API
   useEffect(() => {
@@ -22,10 +23,26 @@ export default function WorkoutList() {
 
   console.log('this is the full list of workouts from state-->', workouts)   // "workouts" is now the full list of workouts we get based off the level we clicked
 
+ // pass in level and exercise
+ const handleClick = (exerciseName) => {
+  fetch(`${process.env.REACT_APP_API_ENDPOINT}/workouts/${level}/${exerciseName}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify({ completed: true })
+  })
+  .then(res => res.json())
+  // .then(data => console.log(data.level.exercise))
+  .catch(err => console.log(err))
+}
+
   return (
     <ul>
-    {workouts.map((exercise, i) => {     // mapping through the list of exercises, setting the data into single variable "exercise"
-      return <li key={i}><Button variant="dark">done</Button> &nbsp; {`name: ${exercise.name}/ sets: ${exercise.sets}/ reps: ${exercise.reps}/`}</li>   // then returning it as a list dynamically by category: DISPLAYING IT TO SITE
+    {workouts.map((exercise, i) => { 
+      // mapping through the list of exercises, setting the data into single variable "exercise"
+      return <li key={i}>
+        <Button variant="dark" onClick={() => handleClick(exercise.name)}>done</Button> &nbsp; {`name: ${exercise.name}/ sets: ${exercise.sets}/ reps: ${exercise.reps}/`}</li>   // then returning it as a list dynamically by category: DISPLAYING IT TO SITE
     })}
   </ul>
   )
